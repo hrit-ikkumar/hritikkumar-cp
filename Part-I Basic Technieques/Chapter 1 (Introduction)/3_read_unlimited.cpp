7651b027f8e1c201a68ae0ee8091542aea90cd91
Hritik Kumar Sharma <hritix@gmail.com>
2020-04-14 21:53:16 +0530

    Read Unlimited Input

---
+++ Part-I Basic Technieques/Chapter 1 (Introduction)/3_read_unlimited.cpp
@@ -0,0 +1,14 @@
+// @hritikkumar
+#include<bits/stdc++.h> // all header files
+using namespace std; //namespace created as std
+
+int main(void)
+{
+	int x; // int type varable 
+	// int can store up to 10^5 values
+	while(cin>>x) // unlimited input in x 
+	{
+		cout<<"X: "<<x<<endl;
+	}
+	return 0; //return type is int
+}
