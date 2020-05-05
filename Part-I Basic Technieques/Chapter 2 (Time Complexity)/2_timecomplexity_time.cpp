dc6b292d5b9162cf43525a9c8b45a19669c26f1e
Hritik Kumar Sharma <hritix@gmail.com>
2020-05-06 05:25:49 +0530

    Time Complexity Part 2

---
+++ Part-I Basic Technieques/Chapter 2 (Time Complexity)/2_timecomplexity_time.cpp
@@ -0,0 +1,23 @@
+// @hritikkumar
+#include<bits/stdc++.h>  // all header files
+
+using namespace std; // namespace created as std
+
+int main(void)
+{
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	clock_t start = clock();
+	#endif
+	
+	//Code Goes Here
+	int iterations = 1e8;
+	while(iterations--);
+	
+	#ifndef ONLINE_JUDGE
+	clock_t end = clock();
+	cout<<"Total Time Used: "<<(double)((end - start)/CLOCKS_PER_SEC)<<endl;
+	#endif
+	return 0;
+}
