b1c1c72e8ef7cb1d4cf1385d622845b2f77f7a29
Hritik Kumar Sharma <hritix@gmail.com>
2020-05-22 04:59:06 +0530

    Ordered and Unordered Map has been added

--- Part-I Basic Technieques/Chapter 4 Data Structures/3_map_ordered_unordered.cpp
+++ Part-I Basic Technieques/Chapter 4 Data Structures/3_map_ordered_unordered.cpp
@@ -1,1 +1,35 @@
+// @hritikkumar
 
+#include<bits/stdc++.h> // all header files
+
+#define endl '\n'
+
+using namespace std; 
+
+int main(void)
+{
+	std::ios::sync_with_stdio(false); cin.tie(0);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	
+	// Map contains key-value pair
+	map<string, int> mp;
+	mp["monkey"] = 4;
+	mp["banana"] = 5;
+	mp["harpisichord"] = 9;
+	
+	cout<<mp["banana"]<<endl; // It will return value of key "banana" string
+	cout<<mp["apple"] <<endl; // It will return 0 because map does not contain "apple" string in it
+	
+	if(mp.count("monkey"))
+	cout<<"monkey consists in mp"<<endl;
+	else 
+	cout<<"monkey does not consist in mp"<<endl;
+	// print all the key and pair in map
+	for(auto pr: mp)
+	{
+		cout<<pr.
+	}
+}
