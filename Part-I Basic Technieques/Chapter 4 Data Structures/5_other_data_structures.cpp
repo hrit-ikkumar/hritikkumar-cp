3360264a64bfb3c567d2984fff7e7394f33c8a97
Hritik Kumar Sharma <hritix@gmail.com>
2020-05-23 19:54:23 +0530

    Bitset has been added

--- Part-I Basic Technieques/Chapter 4 Data Structures/5_other_data_structures.cpp
+++ Part-I Basic Technieques/Chapter 4 Data Structures/5_other_data_structures.cpp
@@ -9,4 +9,20 @@ using namespace std;
 int main(void)
 {
 	std::ios::sync_with_stdio(false); cin.tie(0);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	
+	// Bitset is a data structure which has two values either 0 or 1. It is like an array.
+	bitset<10> bs;
+	cin>>bs;
+	// or bitset<10> bs(string("1010101010")); // 
+	// print the bitset
+	cout<<bs<<endl;
+	cout<<bs[1]<<endl;
+	// you can count all the even parity by using count method
+	cout<<bs.count()<<endl;
+	
+	return 0; // return type is int
 }
