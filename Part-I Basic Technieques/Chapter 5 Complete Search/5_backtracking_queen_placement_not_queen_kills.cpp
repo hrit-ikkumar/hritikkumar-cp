d2367f230da5a267d400feae4d9619e238598143
Hritik Kumar Sharma <hritix@gmail.com>
2020-05-28 05:15:47 +0530

    Backtracking

--- Part-I Basic Technieques/Chapter 5 Complete Search/5_backtracking_queen_placement_not_queen_kills.cpp
+++ Part-I Basic Technieques/Chapter 5 Complete Search/5_backtracking_queen_placement_not_queen_kills.cpp
@@ -6,6 +6,10 @@
 
 using namespace std; //namespace created as std  
 
+void backTrackNQ(long long &, int,int,  int *, int *, int *);
+
+long long backtrackingNQueenPlacment(int );
+
 int main(void)
 {
 	std::ios::sync_with_stdio(false); cin.tie(0);
@@ -15,6 +19,48 @@ int main(void)
 	freopen("output.txt", "w",stdout);
 	#endif
 	
+	int n;
+	cin>>n;
+	long long ans = backtrackingNQueenPlacment(n);
 	
+	cout<<ans<<endl;
+	
+	return 0; // return type is int 
+}
+
+void backTrackNQ(long long &count, int queens, int n, int* col, int *diag1, int *diag2)
+{
+	if(queens == n)
+	{
+		count++;
+		return;
+	}
+	
+	for(int x = 0; x<n;x++)
+	{
+		if(col[x] || diag1[x+queens] || diag2[x-queens + n-1]) continue;
+		
+		col[x] = diag1[x+queens] = diag2[x-queens+n-1] = 1;
+		
+		backTrackNQ(count, queens+1, n, col, diag1, diag2);
+		
+		col[x] = diag1[x+queens] = diag2[x-queens+n-1] = 0;
+	}
 	
 }
+
+long long backtrackingNQueenPlacment(int n)
+{
+	long long count = 0;
+	int* col = new int[n];
+	for(int i=0;i<n;i++) col[i] = 0;
+	int *diag1 = new int[n];
+	for(int i=0;i<n;i++) diag1[i] =0;
+	int *diag2 = new int[n];
+	for(int i=0;i<n;i++)diag2[i] = 0;
+	
+	backTrackNQ(count, 0,n,col, diag1, diag2);
+	return count; // count is the answer of number of possible ways
+	
+}
+
