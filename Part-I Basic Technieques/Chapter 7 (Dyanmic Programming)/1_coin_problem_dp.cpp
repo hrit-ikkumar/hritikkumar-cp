ceed6d2909ae08e962d17a256a418b6997df5de6
Hritik Kumar Sharma <hritix@gmail.com>
2020-06-08 05:06:00 +0530

    DP Solution for coin change problem

--- Part-I Basic Technieques/Chapter 7 (Dyanmic Programming)/1_coin_problem_dp.cpp
+++ Part-I Basic Technieques/Chapter 7 (Dyanmic Programming)/1_coin_problem_dp.cpp
@@ -6,6 +6,8 @@
 
 using namespace std; // namespace created as std
 
+long long int dpMinCoin( vector<long long int>, long long int, long long int, vector<long long int> &, vector<bool> &); // dynamic programming solution by using memorization
+
 long long int recMinCoin(vector<long long int> , long long int, long long int); // recursive function (less efficient)
 
  int main(void)
@@ -24,7 +26,30 @@ long long int recMinCoin(vector<long long int> , long long int, long long int); 
 	cin>>amount;
 	cout<<"Minimum Coins: "<<recMinCoin(coins, n, amount)<<endl;
 	
+	// dp arrays
+	vector<long long int> dp_table(amount+1, 0); // real values
+	vector<bool> dp_ready(amount +1, false); // weather, we have calculated the values or not 
+	cout<<"Minimum Coins: "<<dpMinCoin(coins, n, amount, dp_table, dp_ready)<<endl;
+	
 	return 0; // return type is int  
+}
+
+long long int dpMinCoin(vector<long long int> coins, long long int n, long long int amount, vector<long long int> &dp_table, vector<bool> &dp_ready)
+{
+	if(amount < 0 )
+		return INT_MAX;
+	else if(amount == 0)
+		return 0; 
+	else if(dp_ready[amount]) 
+		return dp_table[amount];
+	
+	long long int best = INT_MAX;
+	for(auto x : coins)
+	{
+		best = min (best , dpMinCoin(coins, n, amount - x, dp_table, dp_ready) + 1);
+	}
+	dp_table[amount] = best, dp_table[amount] = true;
+	return best;
 }
 
 long long int recMinCoin(vector<long long int> coins, long long int n, long long int amount)
