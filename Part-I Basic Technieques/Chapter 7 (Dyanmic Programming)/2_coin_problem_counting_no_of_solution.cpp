c9d7698eb74763551b812d0543a7625a3214df06
Hritik Kumar Sharma <hritix@gmail.com>
2020-06-09 05:18:23 +0530

    Counting the number of solutions of Coin Problem

---
+++ Part-I Basic Technieques/Chapter 7 (Dyanmic Programming)/2_coin_problem_counting_no_of_solution.cpp
@@ -0,0 +1,45 @@
+// @hritikkumar
+
+#include<bits/stdc++.h> // all header files
+
+#define endl '\n'
+
+using namespace std; // namespace created as std
+
+long long int dpCountPossibleCoinCombinations(vector<int>, int , int );
+
+int main(void)
+{
+	std::ios::sync_with_stdio(false); cin.tie(0); // fastio
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout); 
+	#endif
+	
+	int n;
+	cin>>n;
+	vector<int> coins(n);
+	for(int i=0;i<n;i++)cin>>coins[i];
+	int amount;
+	cin>>amount;
+	cout<<"Counts: "<<dpCountPossibleCoinCombinations(coins, n , amount)<<endl;
+	return 0; 
+}
+
+long long int dpCountPossibleCoinCombinations(vector<int> coins, int n, int amount)
+{
+	vector<int> count(amount+1, 0);
+	count[0] = 1;
+	
+	for(int x = 1; x<=amount; ++x)
+	{
+		for(auto c: coins)
+		{
+			if( (x-c) >= 0)
+			{
+				count[x] += count[x-c];
+			}
+		}
+	}
+	return count[amount];
+}
