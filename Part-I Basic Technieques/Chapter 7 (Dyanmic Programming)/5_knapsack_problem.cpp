789b9161bed284c1efa4bd8a153cf9787f414e64
Hritik Kumar Sharma <hritix@gmail.com>
2020-06-13 05:00:06 +0530

    Knapsack Problem DP(Top-Down Approach)

--- Part-I Basic Technieques/Chapter 7 (Dyanmic Programming)/5_knapsack_problem.cpp
+++ Part-I Basic Technieques/Chapter 7 (Dyanmic Programming)/5_knapsack_problem.cpp
@@ -30,7 +30,33 @@ int main(void)
 int knapsackDP(int* weight, int n)
 {
 	int ans = 0;
+	int total_sum = accumulate(weight, weight + n, 0);
+	bool** dp_table = new bool*[total_sum+1];
+	for(int i=0;i<=total_sum;++i)
+		dp_table[i] = new bool[n+1];
 	
+	for(int i=0;i<=total_sum; ++i)
+		for(int j=0;j<=n;j++)
+			dp_table[i][j] = false;
+	
+	
+	dp_table[0][0] = true;
+	for(int k = 1; k<=n; k++)
+	{
+		for(int x = 0; x<=total_sum; ++x)
+		{
+			// weight index will be [k-1] for kth values in our choices
+			if( (x - weight[k-1]) >= 0)
+				dp_table[x][k] |= dp_table[x-weight[k-1]][k-1];
+			dp_table[x][k] |= dp_table[x][k-1];
+		}
+	}
+	
+	for(int i=0; i<=total_sum; ++i)
+	{
+		if(dp_table[i][n])
+			ans +=1;
+	}
 	
 	return ans;
 }
