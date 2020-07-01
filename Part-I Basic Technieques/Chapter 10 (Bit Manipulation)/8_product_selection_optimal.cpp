755a7ffb701e884f00d9cfe3790d795b95f6eb1d
Hritik Kumar Sharma <hritix@gmail.com>
2020-07-01 21:42:22 +0530

    Product Selection Optimal Solution

--- Part-I Basic Technieques/Chapter 10 (Bit Manipulation)/8_product_selection_optimal.cpp
+++ Part-I Basic Technieques/Chapter 10 (Bit Manipulation)/8_product_selection_optimal.cpp
@@ -6,6 +6,8 @@
 
 using namespace std; // namespace created as std
 
+int productSelectionDP(vector<vector<int>> &, int , int );
+
 int main(void)
 {
 	std::ios::sync_with_stdio(false); // fastio
@@ -14,5 +16,32 @@ int main(void)
 	freopen("output.txt", "w", stdout);
 	#endif
 	
+	int k, n;
+	cin>>k>>n;
+	vector<vector<int>> a(k, vector<int>(n));
+	for(int i=0;i<k;i++) for(int j=0;j<n;j++) cin>>a[i][j];
+	int ans = productSelectionDP(a,k,n);
+	cout<<ans<<endl;
+	return 0; // return type is int
+}
+
+int productSelectionDP(vector<vector<int>> &a, int k, int n)
+{
+	vector<vector<int>> dp_table(1<<k, vector<int>(n));
+	for(int x =0;x<k;++x)
+		dp_table[1<<x][0] = a[x][0];
 	
+	for(int d = 1;d<n;d++)
+	{
+		for(int s = 0;s<(1<<k); s++)
+		{
+			dp_table[s][d] = dp_table[s][d-1];
+			for(int x = 0;x<k;x++)
+			{
+				if(s&(1<<x))
+					dp_table[s][d]  = min(dp_table[s][d], dp_table[s^(1<<x)][d-1] + a[x][d]);
+			}
+		}
+	}
+	return dp_table[(1<<k) - 1][n-1];
 }
