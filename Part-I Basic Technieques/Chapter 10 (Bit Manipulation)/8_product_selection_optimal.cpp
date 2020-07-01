2ffd369d5e0c0c76b9ac78e986c321c0aa760e7c
Hritik Kumar Sharma <hritix@gmail.com>
2020-07-01 21:25:09 +0530

    Product Selection Optimal Solution

---
+++ Part-I Basic Technieques/Chapter 10 (Bit Manipulation)/8_product_selection_optimal.cpp
@@ -0,0 +1,18 @@
+// @hritikkumar
+
+#include<bits/stdc++.h> // all header files
+
+#define endl '\n'
+
+using namespace std; // namespace created as std
+
+int main(void)
+{
+	std::ios::sync_with_stdio(false); // fastio
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	
+	
+}
