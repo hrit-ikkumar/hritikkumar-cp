f320ba8ae6d9e4f995c311a31b33e8688bc34830
Hritik Kumar Sharma <hritix@gmail.com>
2020-07-03 07:05:06 +0530

    Depth First Search (DFS)

--- Part-II Graph Algorithms/Chapter 12 (Graph traversal)/1_depth_first_search_dfs.cpp
+++ Part-II Graph Algorithms/Chapter 12 (Graph traversal)/1_depth_first_search_dfs.cpp
@@ -6,6 +6,8 @@
 
 using namespace std; // namespace created as std
 
+void dfs(vector<vector<int>> &, vector<bool> &,vector<int> &, int);
+
 int main(void)
 {
 	std::ios::sync_with_stdio(false); // fastio
@@ -14,5 +16,32 @@ int main(void)
 	freopen("output.txt", "w", stdout); 
 	#endif
 	
+	int vertices, edges;
+	cin>>vertices>>edges;
+	vector<vector<int>> graph(vertices);
+	while(edges--)
+	{
+		int start, end;
+		cin>>start>>end;
+		graph[start].push_back(end);
+		graph[end].push_back(start);
+	}
+	vector<bool> visited(vertices, false);
+	vector<int> ans;
 	
+	dfs(graph, visited, ans, 0);
+	for(int i=0;i<signed(ans.size());i++)
+		cout<<ans[i]<<" ";
+	cout<<endl;
+	return 0; // return type is int 
+}
+
+void dfs(vector<vector<int>> &graph,vector<bool> &visited,vector<int> &ans,  int current)
+{
+	if(visited[current]) return;
+	
+	visited[current] = true;
+	ans.push_back(current);
+	for(auto u: graph[current])
+		dfs(graph, visited, ans, u);
 }
