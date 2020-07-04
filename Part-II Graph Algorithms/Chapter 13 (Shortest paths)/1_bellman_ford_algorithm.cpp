97ab659c93476d0c7f6d9535718c2a548741bd02
Hritik Kumar Sharma <hritix@gmail.com>
2020-07-04 09:45:46 +0530

    Bellman Ford Algorithm for Graph Shortest Path (Doesn't work for negative cycle)

--- Part-II Graph Algorithms/Chapter 13 (Shortest paths)/1_bellman_ford_algorithm.cpp
+++ Part-II Graph Algorithms/Chapter 13 (Shortest paths)/1_bellman_ford_algorithm.cpp
@@ -6,6 +6,8 @@
 
 using namespace std; // namespace created as std
 
+vector<long long int> bellmanFordSPGraph(multiset<tuple<int, int, int>> &, int, int);
+
 int main(void)
 {
 	std::ios::sync_with_stdio(false); // fastio
@@ -13,17 +15,39 @@ int main(void)
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 	#endif
+	// If, There is any negative weight cycle in graph then it won't work!
 	
 	int vertices, edges;
 	cin>>vertices>>edges;
-	vector<vector<pair<int,int>>> graph(vertices);
-	while(edges--)
+	multiset<tuple<int, int ,int>> graph_edges;//undirected & weighted
+	for(int i=0;i<edges;i++)
 	{
 		int start, end, weight;
 		cin>>start>>end>>weight;
-		graph[start].push_back({end, weight});
-		graph[end].push_back({start, weight});
+		graph_edges.insert({weight, start, end});
 	}
 	
+	vector<long long int> ans = bellmanFordSPGraph(graph_edges, vertices, edges);
+	cout<<"Vertices   Distance"<<endl;
+	for(unsigned int i = 0;i<ans.size();i++)
+	{
+		cout<<i<<":         "<<ans[i]<<endl;
+	}
 	return 0; // return type is int
+}
+
+vector<long long int> bellmanFordSPGraph(multiset<tuple<int, int, int>> &graph_edges, int vertices, int edges)
+{
+	vector<long long int> distance(vertices, (long long int) INT_MAX);
+	distance[0] = 0;
+	for(int i=0;i<vertices -1;i++)
+	{
+		for(auto e: graph_edges)
+		{
+			int start, end, weight;
+			tie(weight, start, end) = e;
+			distance[end] = min(distance[end], distance[start] + weight);
+		}
+	}
+	return distance;
 }
