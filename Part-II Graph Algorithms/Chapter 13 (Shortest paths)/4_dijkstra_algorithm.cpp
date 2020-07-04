015263a90ddb76d374c2e2185c2f5190770e9474
Hritik Kumar Sharma <hritix@gmail.com>
2020-07-04 11:52:37 +0530

    Dijkstra Algorithm for Shortest Path First Algorithm (SPFA) in Graph

--- Part-II Graph Algorithms/Chapter 13 (Shortest paths)/4_dijkstra_algorithm.cpp
+++ Part-II Graph Algorithms/Chapter 13 (Shortest paths)/4_dijkstra_algorithm.cpp
@@ -6,6 +6,8 @@
 
 using namespace std; // namespace created as std
 
+vector<long long int> dijkstraSPGraph(vector<vector<pair<int, int>>> &, int , int );
+
 int main(void)
 {
 	std::ios::sync_with_stdio(false); // fastio
@@ -14,6 +16,8 @@ int main(void)
 	freopen("output.txt", "w", stdout);
 	#endif
 	
+	// If, The weight is negative in that case it won't work.
+	
 	int vertices, edges; cin>>vertices>>edges;
 	vector<vector<pair<int, int>>> graph(vertices);
 	for(int i=0;i<edges;i++)
@@ -21,7 +25,34 @@ int main(void)
 		int start, end, weight;
 		cin>>start>>end>>weight;
 		graph[start].push_back({end, weight});
+		graph[end].push_back({start, weight});
 	}
-	
+	vector<long long int> ans = dijkstraSPGraph(graph, vertices, edges);
+	cout<<"Vertices	Distance"<<endl;
+	for(unsigned int i = 0;i<ans.size();i++)
+	cout<<i<<"			"<<ans[i]<<endl;
 	return 0; // return type is int
+}
+
+vector<long long int> dijkstraSPGraph(vector<vector<pair<int, int>>> &graph, int vertices, int edges)
+{
+	vector<bool> visited(vertices, false);
+	vector<long long int> distance(vertices, (long long int) INT_MAX);
+	distance[0] = 0;
+	priority_queue<pair<int, int>> pq;
+	pq.push({0,0}); // source from 0th node
+	while(!pq.empty())
+	{
+		int start = pq.top().second; pq.pop();
+		if(visited[start])	continue;
+		visited[start] = true;
+		for(auto u:graph[start])
+		{
+			int end, end_weight;
+			tie(end, end_weight) = u;
+			distance[end] = min(distance[end], distance[start] + end_weight);
+			pq.push({-distance[end], end});
+		}
+	}
+	return distance;
 }
