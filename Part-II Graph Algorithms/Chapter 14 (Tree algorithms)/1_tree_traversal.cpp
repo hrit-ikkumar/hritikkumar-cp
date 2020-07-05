a53758ca254c19dedfe91b41076263740b07df4f
Hritik Kumar Sharma <hritix@gmail.com>
2020-07-05 12:00:12 +0530

    Tree Traversal

--- Part-II Graph Algorithms/Chapter 14 (Tree algorithms)/1_tree_traversal.cpp
+++ Part-II Graph Algorithms/Chapter 14 (Tree algorithms)/1_tree_traversal.cpp
@@ -6,6 +6,8 @@
 
 using namespace std; 
 
+void dfs(int, int, vector<int> &, vector<vector<int>> &);
+
 int main(void)
 {
 	std::ios::sync_with_stdio(false); // fastio
@@ -13,6 +15,30 @@ int main(void)
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 	#endif
-	
+	int vertices, edges; cin>>vertices>>edges;
+	vector<vector<int>> graph(vertices, vector<int>());
+	for(int i=0;i<edges;i++)
+	{
+		int start, end;
+		cin>>start>>end;
+		graph[start].push_back(end);
+	}
+	vector<int> ans(0);
+	dfs(0,-1, ans, graph);
+	for(unsigned int i = 0;i<ans.size();i++)
+	{
+		cout<<ans[i]<<" ";
+	}
+	cout<<endl;
 	return 0; // return type is int
+}
+
+void dfs(int start, int end, vector<int> &ans, vector<vector<int>> & graph)
+{
+	ans.push_back(start);
+	for(auto u: graph[start])
+	{
+		if(u!=end)
+		dfs(u,start, ans, graph);
+	}
 }
