cbcfbe390ecbf6d94c39166b50346db4099b33d1
Hritik Kumar Sharma <hritix@gmail.com>
2020-07-07 10:03:49 +0530

    Tree Diameter

---
+++ Part-II Graph Algorithms/Chapter 14 (Tree algorithms)/3_tree_diameter.cpp
@@ -0,0 +1,20 @@
+// @hritikkumar
+
+#include<bits/stdc++.h> // all header files
+
+#define endl '\n'
+
+using namespace std; // namespace created as std
+
+int main(void)
+{
+	std::ios::sync_with_stdio(false); // fastio
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.xt", "w", stdout);
+	#endif
+	
+	
+	
+	return 0; // return type is int
+}
