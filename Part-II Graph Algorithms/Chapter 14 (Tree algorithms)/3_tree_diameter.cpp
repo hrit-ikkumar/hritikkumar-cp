f6075d812320498059ca0a00189da0c68643c540
Hritik Kumar Sharma <hritix@gmail.com>
2020-07-08 18:38:22 +0530

    Tree Diameter

--- Part-II Graph Algorithms/Chapter 14 (Tree algorithms)/3_tree_diameter.cpp
+++ Part-II Graph Algorithms/Chapter 14 (Tree algorithms)/3_tree_diameter.cpp
@@ -8,6 +8,7 @@ using namespace std; // namespace created as std
 
 void dfs(vector<vector<int>> &,vector<int> &,  int );
 
+int treeDiameter(vector<vector<int>> &, vector<int> &);
 int main(void)
 {
 	std::ios::sync_with_stdio(false); // fastio
@@ -26,9 +27,8 @@ int main(void)
 	}
 	vector<int> toLeaf(vertices, 0), maxLength(vertices, 0);
 	dfs(graph,toLeaf, 0);
-	for(int x: toLeaf)
-	cout<<x<<" ";
-	cout<<endl;
+	int ans = treeDiameter(graph, toLeaf);
+	cout<<ans<<endl;
 	return 0; // return type is int
 }
 
@@ -39,4 +39,34 @@ void dfs(vector<vector<int>> &graph,vector<int>  &toLeaf, int current)
 		dfs(graph,toLeaf, u);
 		toLeaf[current] = max(toLeaf[current], toLeaf[u] + 1);
 	}
+}
+
+int treeDiameter(vector<vector<int>> &graph, vector<int> &toLeaf)
+{
+	vector<int> height(graph.size(), 0);
+	for(int i=0;i<(signed)graph.size();i++)
+	{
+		if(graph[i].size()==0)
+			height[i] = 0;
+		else if(graph[i].size()==1)
+			height[i] = toLeaf[graph[i][0]] + 1;
+		else
+		{
+			int max1 = toLeaf[graph[i][0]], max2 = INT_MIN;
+			for(int j=1;j<(signed)graph[i].size();j++)
+			{
+				if(toLeaf[graph[i][j]] > max1)
+				{
+					max2 = max1;
+					max1 = toLeaf[graph[i][j]];
+				}
+				else if(toLeaf[graph[i][j]] > max2)
+				{
+					max2 = toLeaf[graph[i][j]];
+				}
+			}
+			height[i] = max1 + max2 + 2;
+		}
+	}
+	return *max_element(height.begin(), height.end());
 }
