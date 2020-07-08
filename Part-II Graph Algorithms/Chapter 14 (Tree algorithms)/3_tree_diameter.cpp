6986d4e7d46799162b75ea8df9e15cfd621e8823
Hritik Kumar Sharma <hritix@gmail.com>
2020-07-08 18:23:51 +0530

    Tree Diameter

--- Part-II Graph Algorithms/Chapter 14 (Tree algorithms)/3_tree_diameter.cpp
+++ Part-II Graph Algorithms/Chapter 14 (Tree algorithms)/3_tree_diameter.cpp
@@ -6,16 +6,18 @@
 
 using namespace std; // namespace created as std
 
+void dfs(vector<vector<int>> &,vector<int> &,  int );
+
 int main(void)
 {
 	std::ios::sync_with_stdio(false); // fastio
 	#ifndef ONLINE_JUDGE
 	freopen("input.txt", "r", stdin);
-	freopen("output.xt", "w", stdout);
+	freopen("output.txt", "w", stdout);
 	#endif
 	int vertices, edges;
 	cin>>vertices>>edges;
-	vector<vector<int>> graph(vertices,vector<int>);
+	vector<vector<int>> graph(vertices,vector<int>());
 	for(int i=0;i<edges;i++)
 	{
 		int start, end;
@@ -23,6 +25,18 @@ int main(void)
 		graph[start].push_back(end);
 	}
 	vector<int> toLeaf(vertices, 0), maxLength(vertices, 0);
-	
+	dfs(graph,toLeaf, 0);
+	for(int x: toLeaf)
+	cout<<x<<" ";
+	cout<<endl;
 	return 0; // return type is int
+}
+
+void dfs(vector<vector<int>> &graph,vector<int>  &toLeaf, int current)
+{
+	for(auto u:graph[current])
+	{
+		dfs(graph,toLeaf, u);
+		toLeaf[current] = max(toLeaf[current], toLeaf[u] + 1);
+	}
 }
