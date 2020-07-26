b2f75260815b8510ad99306ba91de1ef4915253d
Hritik Kumar Sharma <hritix@gmail.com>
2020-07-26 21:23:39 +0530

    Tree Algorithms completed

--- Part-II Graph Algorithms/Chapter 14 (Tree algorithms)/5_all_longest_path.cpp
+++ Part-II Graph Algorithms/Chapter 14 (Tree algorithms)/5_all_longest_path.cpp
@@ -6,9 +6,10 @@
 
 using namespace std; // namespace created as std
 
-void dfs(vector<vector<int>> &,vector<int> &,  int );
+void dfs(vector<vector<int>> &, vector<bool> &,int , int &, int , int & );
 
-int treeDiameter(vector<vector<int>> &, vector<int> &);
+void distance_dfs(vector<vector<int>> &, vector<bool> &, vector<int> &, int, int);
+
 int main(void)
 {
 	std::ios::sync_with_stdio(false); // fastio
@@ -18,57 +19,63 @@ int main(void)
 	#endif
 	int vertices, edges;
 	cin>>vertices>>edges;
-	vector<vector<int>> graph(vertices,vector<int>());
-	for(int i=0;i<edges;i++)
+	vector<vector<int>> graph(vertices, vector<int>(0));
+	for(int i=0;i<edges; i++)
 	{
 		int start, end;
 		cin>>start>>end;
 		graph[start].push_back(end);
 		graph[end].push_back(start);
 	}
-	vector<int> toLeaf(vertices, 0), maxLength(vertices, 0);
+	// diameter of tree => dfs from one node(x) to find the farthest node(a) then again do a dfs from node(a) is node(b) so the answer is distance between a and b (b-a) ans 
+	vector<bool> visited(vertices, false);
+	// first_end  = a, second_end = b
+	int x = 0,a = 0,b = 0, maxCount = 0;
+	dfs(graph, visited, x, a,0, maxCount);
+	visited.assign(visited.size(), false);
+	dfs(graph, visited, a,b, 0, maxCount);
 	
-	dfs(graph,toLeaf, 0);
-	int ans = treeDiameter(graph, toLeaf);
-	cout<<ans<<endl;
-	return 0; // return type is int
+	vector<int> distance_from_a(vertices, 0), distance_from_b(vertices, 0);
+	
+	visited.assign(vertices, false);
+	distance_dfs(graph,visited, distance_from_a, a, 0);
+	
+	visited.assign(vertices, false);
+	distance_dfs(graph, visited, distance_from_b, b, 0);
+	for(int i=0;i<vertices;i++) 
+	cout<<i+1<<" ";
+	cout<<endl;
+	for(int i=0;i<vertices;i++)
+	cout<<max(distance_from_a[i], distance_from_b[i])<<" ";
+	return 0; // return type is int 
 }
 
-void dfs(vector<vector<int>> &graph,vector<int>  &toLeaf, int current)
+void dfs(vector<vector<int>> &graph, vector<bool> &visited, int root,int &farthestRoot, int distance, int &maxDistance)
 {
-	for(auto u:graph[current])
+	visited[root] = true;
+	distance +=1;
+	for(auto u: graph[root])
 	{
-		dfs(graph,toLeaf, u);
-		toLeaf[current] = max(toLeaf[current], toLeaf[u] + 1);
-	}
-}
-
-int treeDiameter(vector<vector<int>> &graph, vector<int> &toLeaf)
-{
-	vector<int> height(graph.size(), 0);
-	for(int i=0;i<(signed)graph.size();i++)
-	{
-		if(graph[i].size()==0)
-			height[i] = 0;
-		else if(graph[i].size()==1)
-			height[i] = toLeaf[graph[i][0]] + 1;
-		else
+		if(!visited[u])
 		{
-			int max1 = toLeaf[graph[i][0]], max2 = INT_MIN;
-			for(int j=1;j<(signed)graph[i].size();j++)
-			{
-				if(toLeaf[graph[i][j]] > max1)
-				{
-					max2 = max1;
-					max1 = toLeaf[graph[i][j]];
-				}
-				else if(toLeaf[graph[i][j]] > max2)
-				{
-					max2 = toLeaf[graph[i][j]];
-				}
-			}
-			height[i] = max1 + max2 + 2;
+			if(distance >= maxDistance)
+			maxDistance = distance, farthestRoot = u;
+			dfs(graph, visited, u, farthestRoot, distance, maxDistance);
 		}
 	}
-	return *max_element(height.begin(), height.end());
+}
+
+void distance_dfs(vector<vector<int>> &graph, vector<bool> &visited, vector<int> &dist, int root, int current_distance)
+{
+	if(visited[root]) return;
+	dist[root] = max(current_distance, dist[root]);
+	visited[root] = true;
+	
+	for(auto u:graph[root])
+	{
+		if(!visited[u])
+		{
+			distance_dfs(graph, visited, dist, u, current_distance +1);
+		}
+	}
 }
