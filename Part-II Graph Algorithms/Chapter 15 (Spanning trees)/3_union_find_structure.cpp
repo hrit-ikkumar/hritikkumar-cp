1ffbcc5efc8e28b9a55039293d36da3b07ad04f1
Hritik Kumar Sharma <hritix@gmail.com>
2020-07-28 11:14:49 +0530

    Union Find Structure

---
+++ Part-II Graph Algorithms/Chapter 15 (Spanning trees)/3_union_find_structure.cpp
@@ -0,0 +1,18 @@
+// @hritikkumar
+
+#include<bits/stdc++.h> // all header files
+
+#define endl '\n';
+
+using namespace std; // namespace created as std
+
+int main(void)
+{
+	std::ios::sync_with_stdio(false); cin.tie(0); cout.tie(0); // fastio
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	
+	return 0; // return type is int
+}
