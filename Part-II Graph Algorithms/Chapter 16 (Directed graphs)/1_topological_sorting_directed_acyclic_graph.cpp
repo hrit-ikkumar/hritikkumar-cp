879f1fd47a17ec48909f304aa4e50bcdba7d82be
Hritik Kumar Sharma <hritix@gmail.com>
2020-07-30 08:13:37 +0530

    Topological Sort using DFS

--- Part-II Graph Algorithms/Chapter 16 (Directed graphs)/1_topological_sorting_directed_acyclic_graph.cpp
+++ Part-II Graph Algorithms/Chapter 16 (Directed graphs)/1_topological_sorting_directed_acyclic_graph.cpp
@@ -6,6 +6,10 @@
 
 using namespace std; // namespace created as std
 
+void topoSortDFS(vector<vector<int>> &, vector<bool> &,int, vector<int> &);
+
+vector<int> topologicalSort(vector<vector<int>> &);
+
 int main(void)
 {
 	std::ios::sync_with_stdio(false); cin.tie(0); cout.tie(0); // fastio
@@ -23,7 +27,37 @@ int main(void)
 		cin>>start>>end;
 		graph[start].push_back(end);
 	}
-	
-	
+	vector<int> topo = topologicalSort(graph);
+	for(int x: topo)
+	cout<<x<<" ";
+	cout<<endl;	
 	return 0; // return type is int
+}
+
+void topoSortDFS(vector<vector<int>> &graph, vector<bool> &visited,int root, vector<int> &ans)
+{
+	visited[root] = true;
+	for(int u: graph[root])
+	{
+		if(!visited[u])
+		{
+			topoSortDFS(graph, visited, u, ans);
+		}
+	}
+	ans.push_back(root);
+}
+
+vector<int> topologicalSort(vector<vector<int>> & graph)
+{
+	vector<int> ans(0);
+	vector<bool> visited(graph.size(), false);
+	for(int i=0;i<(signed)graph.size();i++)
+	{
+		if(!visited[i])
+		{
+			topoSortDFS(graph, visited,i, ans);
+		}
+	}
+	reverse(ans.begin(), ans.end());
+	return ans;
 }
