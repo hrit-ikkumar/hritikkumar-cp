b8e1c9060c9e4a767023929089591bb08627edf2
Hritik Kumar Sharma <hritix@gmail.com>
2020-08-02 05:09:13 +0530

    Counting The Number of Paths in DAG

--- Part-II Graph Algorithms/Chapter 16 (Directed graphs)/2_counting_the_number_of_paths.cpp
+++ Part-II Graph Algorithms/Chapter 16 (Directed graphs)/2_counting_the_number_of_paths.cpp
@@ -6,6 +6,12 @@
 
 using namespace std; // namespace created  as std
 
+void topoSortDFS(vector<vector<int>> &, vector<bool> &,int, vector<int> &);
+
+vector<int> topologicalSort(vector<vector<int>> &);
+
+int countNumberOfPathsDP(vector<vector<int>> &, vector<int> &);
+
 int main(void)
 {
 	std::ios::sync_with_stdio(false); cin.tie(0); cout.tie(0); // fastio
@@ -22,5 +28,50 @@ int main(void)
 		cin>>start>>end;
 		graph[start].push_back(end);
 	}
-	
+	vector<int> topo = topologicalSort(graph);
+	int ans = countNumberOfPathsDP(graph, topo);
+	cout<<ans<<endl;
+}
+
+
+void topoSortDFS(vector<vector<int>> &graph, vector<bool> &visited,int root, vector<int> &ans)
+{
+	visited[root] = true;
+	for(int u: graph[root])
+	{
+		if(!visited[u])
+		{
+			topoSortDFS(graph, visited, u, ans);
+		}
+	}
+	ans.push_back(root);
+}
+
+vector<int> topologicalSort(vector<vector<int>> & graph)
+{
+	vector<int> ans(0);
+	vector<bool> visited(graph.size(), false);
+	for(int i=0;i<(signed)graph.size();i++)
+	{
+		if(!visited[i])
+		{
+			topoSortDFS(graph, visited,i, ans);
+		}
+	}
+	reverse(ans.begin(), ans.end());
+	return ans;
+}
+
+int countNumberOfPathsDP(vector<vector<int>> &graph, vector<int> &topoSort)
+{
+	vector<int> dp_table(graph.size(), 0);
+	dp_table[topoSort[0]] = 1; // We will count the number of nodes
+	for(int i = 0;i<(signed)topoSort.size();i++)
+	{
+		for(int u:graph[topoSort[i]])
+		{
+			dp_table[u] += dp_table[topoSort[i]];
+		}
+	}
+	return dp_table[topoSort.back()];
 }
