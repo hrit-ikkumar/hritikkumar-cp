fd269088ba0c8c85489e3320beda527fb2b42417
Hritik Kumar Sharma <hritix@gmail.com>
2020-08-17 07:38:52 +0530

    Kosaraju's Algorithm

--- Part-II Graph Algorithms/Chapter 17 (Strong connectivity)/1_kosaraju_s_algorithm.cpp
+++ Part-II Graph Algorithms/Chapter 17 (Strong connectivity)/1_kosaraju_s_algorithm.cpp
@@ -6,6 +6,18 @@
 
 using namespace std; // namespace created as std
 
+
+vector<vector<int>> kosarajuAlgorithm(vector<vector<int>> &graph, vector<vector<int>> &reversedGraph)
+{
+	vector<vector<int>> ans(0);
+	vector<bool> visited(graph.size(), false);
+	stack<int> stk;
+	
+	
+	
+	return ans;
+}
+
 int main(void)
 {
 	std::ios::sync_with_stdio(false); cin.tie(0); cout.tie(0); // fastio
@@ -16,7 +28,23 @@ int main(void)
 	
 	int vertices, edges;
 	cin>>vertices>>edges;
-	
-	
+	vector<vector<int>> graph(vertices, vector<int>(0)), reversedGraph(vertices, vector<int>(0));
+	for(int i=0;i<edges;i++)
+	{
+		int start, end;
+		cin>>start>>end;
+		graph[start].push_back(end);
+		reversedGraph[end].push_back(end);
+	}
+	vector<vector<int>> ans = kosarajuAlgorithm(graph, reversedGraph);
+	for(int i=0;i<(signed)ans.size(); i++)
+	{
+		cout<<i<<": {";
+		for(int j=0;j<(signed)ans[i].size(); ++j)
+		{
+			cout<<ans[i][j]<<" ";
+		}
+		cout<<"}"<<endl;
+	}
 	return 0; // return type is int 
 }
