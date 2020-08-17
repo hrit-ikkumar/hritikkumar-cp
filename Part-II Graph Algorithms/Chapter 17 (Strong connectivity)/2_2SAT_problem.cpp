f153132ef1afe27ec20245acdc8f28de86b0c730
Hritik Kumar Sharma <hritix@gmail.com>
2020-08-17 11:02:59 +0530

    2SAT Problem

---
+++ Part-II Graph Algorithms/Chapter 17 (Strong connectivity)/2_2SAT_problem.cpp
@@ -0,0 +1,20 @@
+// @hritikkumar
+
+#include<bits/stdc++.h> // all header files
+
+#define endl '\n'
+
+using namespace std; // namespace created as std
+
+int main(void)
+{
+	std::ios::sync_with_stdio(false); cin.tie(0); cout.tie(0); // fastio
+	#ifdef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	 
+	int n;
+	cin>>n;
+	
+}
